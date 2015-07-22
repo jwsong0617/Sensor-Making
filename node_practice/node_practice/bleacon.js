5455e1178da9aace25c596a55fafcebd9f53ac9e
jwsong0617 <jwsong0617@gmail.com>
2015-07-22 15:19:21 +0900

    bleacon

---
+++ node_practice/node_practice/bleacon.js
@@ -0,0 +1,42 @@
+﻿var noble = require('bleacon');
+Bleacon.startScanning();
+
+Bleacon.on('discover', function (bleacon) {
+    if (bleacon) {
+        console.log("bleacon discoverd: " + bleacon.uuid + "\t" + bleacon.rssi+"\t"+bleacon.proximity);
+    }
+    /*
+    //var ib = new ibeacon(bleacon.uuid);
+    bleacon.on('connect', function () {
+        console.log('connected' + bleacon.uuid + "\t" + bleacon.rssi);
+    });
+    bleacon.on('disconnect', function () {
+        console.log('disconnected' + bleacon.uuid + "\t" + bleacon.rssi);
+    });
+
+    //bleacon.on('rssiUpdate', writeout);//updateRSSI
+    
+    /*
+    setInterval(function () {
+        bleacon.updateRssi();
+    }, 2000);
+     * */
+});
+setInterval(function () {
+    Bleacon.startScanning();
+}, 2000);
+/*
+function writeout(rssi) {
+    console.log(rssi);
+}
+function ibeacon(uuid) {
+    var id = uuid;
+    this.get_uuid = function () {
+        return this.id;
+    }
+};
+
+ibeacon.prototype.archiveRSSI = function (rssi) {
+    console.log(rssi);
+};
+ * */

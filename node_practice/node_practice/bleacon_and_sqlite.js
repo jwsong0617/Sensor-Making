d7d1528aa9e9965b4e401712d883524fa0525590
jwsong0617 <jwsong0617@gmail.com>
2015-07-22 21:40:06 +0900

    sqlite_timestamp

--- node_practice/node_practice/bleacon_and_sqlite.js
+++ node_practice/node_practice/bleacon_and_sqlite.js
@@ -21,28 +21,17 @@ Bleacon.on('discover', function (bleacon) {
         //console.log("bleacon discoverd: " + bleacon.uuid + "\t" + bleacon.rssi + "\t" + bleacon.accuracy);
         db.serialize(function () {
             if (!exists) {
-                db.run("CREATE TABLE IBEACON (timestamp TEXT PRIMARY KEY,uuid TEXT,distance REAL)");
-            }
-            
-            var stmt = db.prepare("INSERT INTO IBEACON VALUES (?,?,?)");
-            var d = new Date();
-            var n = d.toLocaleTimeString();  
-            stmt.run(n,bleacon.uuid,bleacon.rssi);
-            //Insert random data
-            /*
-            var rnd;
-            for (var i = 0; i < 10; i++) {
-                rnd = Math.floor(Math.random() * 10000000);
-                stmt.run("Thing #" + rnd);
-            }
-            */
+                db.run("CREATE TABLE IBEACON (Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,UUID TEXT,Distance REAL)");
+            }            
+            var stmt = db.prepare("INSERT INTO IBEACON VALUES (?,?)");
+            stmt.run(bleacon.uuid,bleacon.accuracy);
             stmt.finalize();
-
+            /*
             db.each("SELECT timestamp,uuid,distance FROM IBEACON", function (err, row) {
                 console.log(row.timestamp, row.uuid, row.distance);
             });
+            */
         });
-
     }
 });
 
--- node_practice/node_practice/sqlite_query_test.js
+++ node_practice/node_practice/sqlite_query_test.js
@@ -2,7 +2,7 @@
 var dbfile = "./beacon.db";
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(dbfile);
-setInterval(db.each("SELECT timestamp,uuid,distance FROM IBEACON", function (err, row) {
-    console.log(row.timestamp, row.uuid, row.distance);
+setInterval(db.each("SELECT uuid,distance FROM IBEACON", function (err, row) {
+    console.log(row.uuid, row.distance);
 }), 10000);
 

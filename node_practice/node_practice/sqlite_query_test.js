611190a1ee89ef991749fd4207fde1a0a002d88c
jwsong0617 <jwsong0617@gmail.com>
2015-07-23 17:17:23 +0900

    sql_test_2

--- node_practice/node_practice/sqlite_query_test.js
+++ node_practice/node_practice/sqlite_query_test.js
@@ -4,7 +4,12 @@ var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(dbfile);
 
 var stmt = "SELECT Timestamp,UUID,Distance from IBEACON where UUID='e2c56db5dffb48d2b060d0f5a71096e0'";
+/*
 db.all(stmt, function (err, rows) {
 //rows contain values while errors, well you can figure out.
-    console.log(rows.uuid,rows.);
+    console.log(rows.Timestamp,rows.UUID,rows.Distance);
+});
+*/
+db.each(stmt, function (err, rows) {
+    console.log(rows.Timestamp + " " + rows.UUID + ", " + rows.Distance);
 });

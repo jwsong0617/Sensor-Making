f976e73d52b93dd713f972979e9dd68eb26799b4
jwsong0617 <jwsong0617@gmail.com>
2015-07-27 10:16:15 +0900

    dbmanager_v0.1

---
+++ node_practice/node_practice/dbmanager.js
@@ -0,0 +1,100 @@
+﻿var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+
+//create DB
+var exists;
+exports.createDBfile = function (filename){
+    var dbfile = "./" + filename + ".db";
+    var exists = fs.existsSync(dbfile);    
+    if (!exists) {
+        console.log("Creating DB file.");
+        fs.openSync(dbfile, "w");
+    }
+    if (exists) {
+        console.log("DB file already"+filename+" exists")
+    }
+}
+//get DB
+exports.getDB = function (filename){
+    var dbfile = "./" + filename+".db";
+    var exists = fs.existsSync(dbfile);
+    if(exists){    
+        //open the database
+        var db = new sqlite3.Database(dbfile);
+        return db;
+        }            
+    else {
+        console.log("Database file does not exists")
+    }
+}
+//create BeaconTable
+exports.createBeaconTable = function (db, name){
+    if(exists){
+        query = "CREATE TABLE "+name+" (Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP PRIMARY KEY NOT NULL,UUID TEXT NOT NULL,Distance REAL NOT NULL)"
+        db.run(query);
+    }
+}
+
+//insert BeaconData
+exports.insertBeaconData = function (db,table,timestamp,uuid,distance){
+    if (exists) {
+        db.serialize(function () {
+            var query = "INSERT INTO "+table+"(Timestamp,UUID,Distance) VALUES (?,?,?)";
+            var stmt = db.prepare(query);
+            stmt.run(timestamp,uuid,distance, function () {
+                console.log(timestamp+", "+uuid+", "+distance);
+            });
+            stmt.finalize();
+        });
+    }
+}
+
+//get data depends on condition
+exports.getBeaconData = function (db,table,condition,callback) {
+    if (exists) {
+        db.serialize(function () {
+            //condition은 사용자에게 입력받은 조건(where절)
+            var stmt = "SELECT * from "+table+" where ";
+            stmt = stmt + condition;
+            db.all(stmt, function (err, rows) {
+                if (err) throw err;
+                if (rows.length != 0) {
+                    rows.forEach(function (row) {
+                        //print out results
+                        console.log(row.Timestamp + " " + row.UUID + ", " + row.Distance);
+                    });
+                    callback(rows);//rows는 array
+                }
+                else {
+                    console.log("Data dose not exists");
+                }
+            });
+        })
+    }
+    else {
+        console.log("Database file does not exists")
+    }
+}
+/*
+exports.setBeaconDataArray = function (rows) {
+    if (exists) {
+        if (rows.length != 0) {
+            var array = new Array(new Array(rows.length), new Array(3));
+            var i = 0;
+            var j = 0;
+            rows.forEach(function (row) {
+                array[i][0] = row.Timestamp;
+                array[i][1] = row.UUID
+                array[i][2] = row.Distance;
+                i++;
+                //print out results
+                //console.log(row.Timestamp + " " + row.UUID + ", " + row.Distance);
+            });
+        }
+    }     
+}
+*/
+//close db
+exports.closeDB = function (db){
+    db.close();
+}
--- node_practice/node_practice/sqlite_query_test.js
+++ node_practice/node_practice/sqlite_query_test.js
@@ -8,8 +8,30 @@ fs.exists(dbfile, function (exists) {
         //open the database
         var db = new sqlite3.Database(dbfile);
         db.serialize(function () {
-            var stmt = "SELECT Timestamp,UUID,Distance from IBEACON where ";
+            var stmt = "SELECT * from IBEACON where ";
+            /*
+            //UUID선택
             var condition = "UUID = 'e2c56db5dffb48d2b060d0f5a71096e0'";
+              
+            //거리 선택
+            var a = 2
+            var b = 4
+            //2~4미터 사이 거리            
+            var condition = "Distance >= " + a + " AND Distance <= " + b;
+            */
+            //구간 선택
+            //var date1 = "07" + "-" + "22" + "-" + "2015";
+            //var date2 = "07" + "-" + "23" + "-" + "2015";
+            var date3 = '22 Jul 2015 00:00:00 GMT+0900';
+            var date4 = '23 Jul 2015 00:00:00 GMT+0900';
+            //var d1 = new Date(date1).getTime();
+            //var d2 = new Date(date2).getTime();
+            var d3 = Date.parse(date3);
+            var d4 = Date.parse(date4);
+            var condition = "Timestamp >= " + d3 + " AND Timestamp <= " + d4;
+            //Wed Jul 22 2015 00:00:00 GMT+0900 (대한민국 표준시) 부터
+            //Thu Jul 23 2015 00:00:00 GMT+0900(대한민국 표준시) 까지             
+            //사용자에게 입력받은 조건
             stmt = stmt + condition;
             db.all(stmt, function (err, rows) {
                 if (err) throw err;
@@ -17,7 +39,7 @@ fs.exists(dbfile, function (exists) {
                     rows.forEach(function (row) {
                         //print out results
                         console.log(row.Timestamp + " " + row.UUID + ", " + row.Distance);
-                    });                    
+                    });
                 }
                 else {
                     console.log("Database dose not exists");
@@ -26,7 +48,6 @@ fs.exists(dbfile, function (exists) {
         })
     }
 });
-
 /*
 db.all(stmt, function (err, rows) {
 //rows contain values while errors, well you can figure out.

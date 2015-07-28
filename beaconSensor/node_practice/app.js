0b1fb553e67c655697c543963bbab0a502ee743a
jwsong0617 <jwsong0617@gmail.com>
2015-07-28 14:18:11 +0900

    query test 1

--- beaconSensor/node_practice/app.js
+++ beaconSensor/node_practice/app.js
@@ -1,11 +1,12 @@
-﻿var dbmanagerTest = require('./dbmanagerTest.js');
+﻿var dbmanager = require('./dbmanager.js');
 var ibeacon = require('./ibeacon.js');
 
-ibeaconDB = dbmanagerTest.getDataBase('ibeacon');
+ibeaconDB = dbmanager.getDataBase('ibeacon');
 /*
 ibeaconDB.createTable('ibeacon');
 ibeacon.addBeaconListener(function (beacon, timestamp) {
     ibeaconDB.insertData('ibeacon', timestamp, beacon.uuid, beacon.accuracy);
 });
 */
-ibeaconDB.getData('ibeacon')
+ibeaconDB.getData('ibeacon', 'Timestamp == 1438060516877');
+ibeaconDB.getData('ibeacon',3)

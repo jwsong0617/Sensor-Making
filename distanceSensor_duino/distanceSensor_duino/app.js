5258f58d5275ff348411f32191a9e72f33819b60
jwsong0617 <jwsong0617@gmail.com>
2015-07-28 10:15:02 +0900

    setInterval message request

--- distanceSensor_duino/distanceSensor_duino/app.js
+++ distanceSensor_duino/distanceSensor_duino/app.js
@@ -9,8 +9,13 @@ var arduino = require('duino'),
 board.on('ready', function () {
     console.log("arduino board is ready to serve.");
     board.pinMode(pinName, 'in');
+
+    setInterval(function () {
+        console.log('request to get an analog value from ' + pinName);
+        board.analogRead(pinName);
+    }, 1000);
 });
 
 board.on('data', function (message) {
-    console.log(m);
+    console.log(message);
 });

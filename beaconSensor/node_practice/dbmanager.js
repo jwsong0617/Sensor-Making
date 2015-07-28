680e5d42cfe18c8f97bc36c44d8475786f74869f
jwsong0617 <jwsong0617@gmail.com>
2015-07-28 15:43:27 +0900

    dbmanager final test

--- beaconSensor/node_practice/dbmanager.js
+++ beaconSensor/node_practice/dbmanager.js
@@ -4,7 +4,7 @@ var sqlite3 = require("sqlite3").verbose();
 exports.getDataBase = function (type) {
     if (type == 'ibeacon') return new ibeaconDB();
     else if (type == 'distance') return new distanceDB();
-} 
+}
 
 exports.closeDataBase = function (database) {
     database.close();
@@ -20,50 +20,48 @@ function ibeaconDB() {
             fs.openSync(dbfile, "w");
             console.log("Done.");
             console.log("Getting DB file.");
-            var ibeaconDataBase = new sqlite3.Database(dbfile);            
-            return ibeaconDataBase;            
+            var ibeaconDataBase = new sqlite3.Database(dbfile);
+            return ibeaconDataBase;
         }    
         else if (exists) {
             //open the database
             console.log("Getting DB file.");
-            var ibeaconDataBase = new sqlite3.Database(dbfile);            
-            return ibeaconDataBase;            
+            var ibeaconDataBase = new sqlite3.Database(dbfile);
+            return ibeaconDataBase;
         }
-        console.log("Done.");                    
+        console.log("Done.");
     }();
 }
 
-ibeaconDB.prototype.createTable = function (name) {    
+ibeaconDB.prototype.createTable = function (name) {
     query = "CREATE TABLE " + name + " (Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP PRIMARY KEY NOT NULL,UUID TEXT NOT NULL,Distance REAL NOT NULL)"
     this.database.run(query);
 }
-ibeaconDB.prototype.getData = function (table, condition) {
+ibeaconDB.prototype.getQueryResult = function (table, callback, condition) {
     var db = this.database;
-    var results = new Array();
     db.serialize(function () {
         //condition은 사용자에게 입력받은 조건(where절)
-        if(typeof condition === 'string'){
+        if (typeof condition === 'string') {
             var stmt = "SELECT * from " + table + " where " + condition;
-            //stmt = stmt + condition;
+            //stmt = stmt + condition;      
         }
         else {
             var stmt = "SELECT * from " + table;
-        }        
+        }
         db.all(stmt, function (err, rows) {
             if (err) throw err;
             if (rows.length != 0) {
-                results = rows.slice();//rows는 array                
+                callback(rows);//rows는 array              
             }
             else {
                 console.log("Data dose not exists");
             }
-        });                    
+        });
     })
-    return results;
 }
 //insert BeaconData
-ibeaconDB.prototype.insertData = function (table, timestamp, uuid, distance) {
-    var db = this.database;        
+ibeaconDB.prototype.insertSignalData = function (table, timestamp, uuid, distance) {
+    var db = this.database;
     db.serialize(function () {
         var query = "INSERT INTO " + table + "(Timestamp,UUID,Distance) VALUES (?,?,?)";
         var stmt = db.prepare(query);
@@ -71,7 +69,7 @@ ibeaconDB.prototype.insertData = function (table, timestamp, uuid, distance) {
             console.log(timestamp + ", " + uuid + ", " + distance);
         });
         stmt.finalize();
-    });    
+    });
 }
 /*
 function distanceDB() {

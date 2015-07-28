cb9498c135ccd17c2aeeea15a3e1a3f23e703ed0
jwsong0617 <jwsong0617@gmail.com>
2015-07-28 10:02:17 +0900

    duino blink test

--- distanceSensor_duino/distanceSensor_duino/app.js
+++ distanceSensor_duino/distanceSensor_duino/app.js
@@ -1,18 +1,11 @@
-﻿var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-
-db.serialize(function () {
-    db.run("CREATE TABLE lorem (info TEXT)");
-    
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-    
-    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-        console.log(row.id + ": " + row.info);
+﻿var arduino = require('duino'),
+    board = new arduino.Board({
+        device: 'USB0'
     });
+
+var led = new arduino.Led({
+    board: board,
+    pin: 13
 });
 
-db.close();
+led.blink();

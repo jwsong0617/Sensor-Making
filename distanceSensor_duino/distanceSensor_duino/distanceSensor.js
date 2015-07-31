1854061773c743dc53ded891cb1e5a2703036e32
jwsong0617 <jwsong0617@gmail.com>
2015-07-31 18:06:19 +0900

    removelistener test

--- distanceSensor_duino/distanceSensor_duino/distanceSensor.js
+++ distanceSensor_duino/distanceSensor_duino/distanceSensor.js
@@ -4,7 +4,7 @@ var arduino = require('duino'),
     board = new arduino.Board({
         device: 'USB0'
     });
-//var ready = false;
+var remove = false;
 
 /*
 exports.addSensorListener = function (callback) {
@@ -46,41 +46,43 @@ exports.addDistanceListener = function (callback) {
 }
 */
 exports.removeDistanceListener = function () {
-    ready = false;
+    remove = true;
 }
 exports.addSensorListener = function (callback) {
-    board.on('ready', function () {
-        console.log("arduino board is ready to serve.");
-        board.pinMode(pinName, 'in');
+    if (!remove) {
+        board.on('ready', function () {
+            console.log("arduino board is ready to serve.");
+            board.pinMode(pinName, 'in');
+            
+            setInterval(function () {
+                //console.log('request to get an analog value from ' + pinName);
+                board.analogRead(pinName);
+            }, 1000);
+        });
         
-        setInterval(function () {
-            //console.log('request to get an analog value from ' + pinName);
-            board.analogRead(pinName);
-        }, 1000);
-    });
-    
-    board.on('connected', function () {//connect checked
-        console.log('serial port is connected');
-    });
-    
-    
-    board.on('data', function (message) {
-        //    console.log(message);
-        var m = message.slice(0, -1).split('::'),
-            err = null,
-            pin, data;
+        board.on('connected', function () {//connect checked
+            console.log('serial port is connected');
+        });
         
-        if (!m.length) {
-            return;
-        }
         
-        pin = m[0];
-        data = m.length === 2 ? m[1] : null;
-        
-        //console.log('reading value from pin' + pin + ': ' + data);
-        if (pin === pinName) {
-            var cm = 10650.08 * Math.pow(data, -0.935) - 10;
-            console.log('current distance is ' + cm + ' cm');
-        }
-    });
+        board.on('data', function (message) {
+            //    console.log(message);
+            var m = message.slice(0, -1).split('::'),
+                err = null,
+                pin, data;
+            
+            if (!m.length) {
+                return;
+            }
+            
+            pin = m[0];
+            data = m.length === 2 ? m[1] : null;
+            
+            //console.log('reading value from pin' + pin + ': ' + data);
+            if (pin === pinName) {
+                var cm = 10650.08 * Math.pow(data, -0.935) - 10;
+                console.log('current distance is ' + cm + ' cm');
+            }
+        });
+    }
 }
